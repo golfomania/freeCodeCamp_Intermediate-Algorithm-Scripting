10f0121b7a6798450ff0fac52590f391663e9cad
Martin Loeffler <ML.Loeffler@gmx.de>
2019-06-16 22:56:33 +0200

    15.06.2019

---
+++ 17_Everything Be True.js
@@ -0,0 +1,13 @@
+function truthCheck(collection, pre) {
+  return collection.every(el => Boolean(el[pre]) == true);
+}
+
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" }
+  ],
+  "sex"
+);
